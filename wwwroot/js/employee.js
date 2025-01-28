3b98a58a66f7ef2e4a2d643a9d58fb22fe1ceb22
France Samaniego <france.samaniego@odecci.com>
2025-01-28 13:25:53 +0800

    Address

--- wwwroot/js/employee.js
+++ wwwroot/js/employee.js
@@ -40,10 +40,11 @@ var addressCondition = 0;
             $("#province").append('<option value="0" disabled selected>Select Province</option>');
             for (var i = 0; i < array.length - 1; i++) {
                 let headers = array[i].split(",");
-                console.log(headers[2].replaceAll('"', ''));
-                console.log((headers[3].replaceAll('"', '')) + " == " + region);
-                console.log((headers[3].replaceAll('"', '')) + " == " + region.replaceAll('"', ''));
+                //console.log(headers[2].replaceAll('"', ''));
+                //console.log((headers[3].replaceAll('"', '')) + " == " + region);
+                //console.log((headers[3].replaceAll('"', '')) + " == " + region.replaceAll('"', ''));
                 if (headers[3].replaceAll('"', '') == region) {
+                    console.log(headers[2].replaceAll('"', ''));
                     $("#province").append('<option value="' + headers[2].replaceAll('"', '') + '"  data-id="' + headers[4].replaceAll('"', '') + '">' + headers[2].replaceAll('"', '') + "</option>");
                 }
 
